e3db15951722f5a091acd90e88727648a9cf2dfb
Antoan Antoniev <aantonie@2-B-1.42heilbronn.de>
2023-10-16 15:58:42 +0200

    mojor change - 20 ready y norm

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,9 +1,46 @@
-// 1st - determine the length of the dest string with the help of strlen function
-// Why - determine how much space is available in the destination buffer.
-// 2nd - calculate the space remaining in the destination buffer(dstsize)
-// 0alculate the total length of the concatenated string(incl. null-terminator)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aantonie <aantonie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 15:21:37 by aantonie          #+#    #+#             */
+/*   Updated: 2023/10/16 15:48:51 by aantonie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h> // Include the header to resolve size_t issues
+
+static size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/*
+The strlcpy() and strlcat() functions copy and concatenate strings with the same 
+input parameters and output result as snprintf(3).  They are designed to be 
+safer, more consistent, and less error prone replacements for the easily misused 
+functions strncpy(3) and strncat(3).
+strlcpy() and strlcat() take the full size of the destination buffer and 
+guarantee NUL-termination if there is room.  Note that room for the NUL 
+should be included in dstsize.
+strlcat() appends string src to the end of dst.  It will append at most dstsize
+ - strlen(dst) - 1 characters.  It will then NUL-terminate, unless dstsize is 0
+or the original dst string was longer than dstsize (in practice this should not
+happen as it means that either dstsize is incorrect or that dst is not a proper
+ string). If the src and dst strings overlap, the behavior is undefined.
+ return
+ strlcat() functions return the total length of the string they tried to create
+*/
 
 size_t	ft_strlcat(char *dest, const char *source, size_t destsz)
 {
@@ -25,23 +62,3 @@ size_t	ft_strlcat(char *dest, const char *source, size_t destsz)
 		dest[i1 + i2] = '\0';
 	return (i1 + ft_strlen(source));
 }
-
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char dest[10] = "Hello,";
-    const char *src = " World!";
-
-    size_t result = strlcat(dest, src, sizeof(dest));
-    size_t result1 = ft_strlcat(dest, src, sizeof(dest));
-
-    printf("Concatenated string: %s\n", dest);
-    printf("Total length of concatenated string %zu\n", result);
-
-    printf("Concatenated string: %s\n", dest);
-    printf("Total length of concatenated string %zu\n", result1);
-
-    return 0;
-}
-
