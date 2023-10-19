af94537856c8831e61bfbb748dc3bf4d6437385f
Antoan Antoniev <aantonie@3-F-6.42heilbronn.de>
2023-10-19 18:29:41 +0200

    so far so good

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,35 +6,56 @@
 /*   By: aantonie <aantonie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 22:22:23 by aantonie          #+#    #+#             */
-/*   Updated: 2023/10/15 01:21:53 by aantonie         ###   ########.fr       */
+/*   Updated: 2023/10/19 12:14:09 by aantonie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// What does memmove do? -> copies len bytes from string src to string dst
+// 1. sizeof(int) * 10 - the size of 1 int times 10(in general)
+// 2. memmove() can handle overlapping destination and source blocks of memory
+// 3. otherwise memcpy is better to use
+
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	int	i;
+	size_t			t;
+	unsigned char	*dest;
+	unsigned char	*source;
 
-	if (!dst || !src)
-		return (NULL);
+	dest = (unsigned char *)dst;
+	source = (unsigned char *)src;
+	t = len;
 	if (dst > src)
 	{
-		i = (int)len - 1;
-		while (i >= 0)
+		while (t > 0)
 		{
-			*(char *)(dst + i) = *(char *)(src + i);
-			i--;
+			dest[t - 1] = source[t - 1];
+			t--;
 		}
 	}
-	else
-	{
-		i = 0;
-		while (i < (int)len)
-		{
-			*(char *)(dst + i) = *(char *)(src + i);
-			i++;
-		}
-	}
+	else if (src > dst)
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+// 	int source[10]= {1,2,3,4,5,6,7,8,9,10};
+// 	int destination[10];
+
+// 	memmove(destination, source, sizeof(int) * 10);
+
+// 	int i;
+
+// 	i = 0;
+// 	while (i < 10)
+// 	{
+
+// 	}
+
+// 	return 0;
+// }
