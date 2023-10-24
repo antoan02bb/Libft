2daa2ea859eed5c08f11b659915adfea6f5181d4
Antoan Antoniev <aantonie@2-C-5.42heilbronn.de>
2023-10-24 19:55:02 +0200

    Libft 2nd Part

---
+++ ft_substr.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+
+// Allocates (with malloc(3)) and returns a substring
+// from the string ’s’.
+// The substring begins at index ’start’ and is of
+// maximum size ’len’.
+
+// char const *s -> input string
+// char 
+
+#include "libft.h"
+
+char *ft_substr(char const *string, unsigned int start, size_t len) {
+	// Handle invalid input
+	return '0';
+}
